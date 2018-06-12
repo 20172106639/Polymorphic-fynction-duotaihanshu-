f2af2d62d9d83ab21e2739ef520978b17fca1567
20172106639 <1757177667qq.com>
2018-06-12 15:14:05 +0800

    第二次上传

--- 多态函数/多态函数.cpp
+++ 多态函数/多态函数.cpp
@@ -2,10 +2,65 @@
 //
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
+class CFeet
+{
+protected:
+	int inches;
+	int feet;
+public:
 
+	CFeet();
+	void setinches(int i);
+	void setfeet(int f);
+	virtual void display();
+	int getfeet();
+	int getinches();
+};
 
+CFeet::CFeet()
+{
+	inches = 0;
+	feet = 0;
+}
+void CFeet::setfeet(int f)
+{
+	feet = f;
+}
+void CFeet::setinches(int i)
+{
+	inches = i % 12;
+	feet = feet + i / 12;
+}
+void CFeet::display()
+{
+	cout << "»ùÀà" << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
+}
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinches()
+{
+	return inches;
+}
+class CMyFeet :public CFeet
+{
+public:
+	void display();
+};
+void CMyFeet::display()
+{
+	cout << "ÅÉÉúÀà" << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
+}
 int main()
 {
-    return 0;
+	CFeet *p;
+	p = new CMyFeet;// //CMyFeet Afeet;p=&Afeet;
+	p->setfeet(3);
+	p->setinches(8);
+	p->display();
+	p->CFeet::display();
+	return 0;
 }
-
